975b150b4ac339402e30f6701ddec31ef7d9298a
sudonym-i <isickln08@gmail.com>
2026-01-29 15:38:20 -0700

    Issue-8: begin work on a mymru.ca login message
    to view current progress, start dev server and open http://127.0.0.1:3000/login_mru/show

---
+++ NIPS/test/controllers/login_mru_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class LoginMruControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get login_mru_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get login_mru_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get login_mru_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get login_mru_create_url
+    assert_response :success
+  end
+end
